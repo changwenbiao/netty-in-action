837ca7f2c25bac1ec4631e7edd58329e87d34583
changwenbiao <422002381@qq.com>
2020-06-18 23:22:46 +0800

    first commit

--- chapter6/src/main/java/nia/chapter6/ChannelFutures.java
+++ chapter6/src/main/java/nia/chapter6/ChannelFutures.java
@@ -3,6 +3,7 @@ package nia.chapter6;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.socket.nio.NioSocketChannel;
 
